a520dd1deb604d499bd137637fd38caf25cf1b39
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-04-28 14:16:45 +0200

    Iterated to display all pokemon names and highlighted special largest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,41 @@
-let pokemonList=[
-{
-  name:'Bulbasaur',
-  height:2,
-  types:['grass','poison'],
-  category:'Seed'
-},
-{
-  name:'Ivysaur',
-  height:3,
-  types:['grass','poison'],
-  category:'Seed'
-},
-{
-  name:'Venusaur',
-  height:6,
-  types:['grass','poison'],
-  category:'Seed'
-},
-{
-  name:'Charmander',
-  height:2,
-  types:['fire'],
-  category:'Lizard'
-} ];
+// Array of Pokemon objects
+const pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 2,
+    types: ['grass', 'poison'],
+    category: 'Seed'
+  },
+  {
+    name: 'Ivysaur',
+    height: 3,
+    types: ['grass', 'poison'],
+    category: 'Seed'
+  },
+  {
+    name: 'Venusaur',
+    height: 6,
+    types: ['grass', 'poison'],
+    category: 'Seed'
+  },
+  {
+    name: 'Charmander',
+    height: 2,
+    types: ['fire'],
+    category: 'Lizard'
+  },
+  {
+    name: 'Charizard',
+    height: 5,
+    types: ['fire', 'flying'],
+    category: 'Flame'
+  }
+]
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 5) { // Highlighting the special big pokemon in the list
+    document.write('<p class="special">' + pokemonList[i].name, '(height: ' + pokemonList[i].height + ')-Wow!-that\'s big</p>')
+  } else {
+    document.write('<p>' + pokemonList[i].name, '(height: ' + pokemonList[i].height + ')</p>')
+  }
+}
