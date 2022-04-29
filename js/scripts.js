b0ea41b130f6d41639bd16447afa39241e1a1036
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-04-29 11:59:23 +0200

    variable,function inside IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,52 @@
-// Array of Pokemon objects
-const pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 2,
-    types: ['grass', 'poison'],
-    category: 'Seed'
-  },
-  {
-    name: 'Ivysaur',
-    height: 3,
-    types: ['grass', 'poison'],
-    category: 'Seed'
-  },
-  {
-    name: 'Venusaur',
-    height: 6,
-    types: ['grass', 'poison'],
-    category: 'Seed'
-  },
-  {
-    name: 'Charmander',
-    height: 2,
-    types: ['fire'],
-    category: 'Lizard'
-  },
-  {
-    name: 'Charizard',
-    height: 5,
-    types: ['fire', 'flying'],
-    category: 'Flame'
+const pokemonRepository = (function () {
+  const pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 2,
+      types: ['grass', 'poison'],
+      category: 'Seed'
+    },
+    {
+      name: 'Ivysaur',
+      height: 3,
+      types: ['grass', 'poison'],
+      category: 'Seed'
+    },
+    {
+      name: 'Venusaur',
+      height: 6,
+      types: ['grass', 'poison'],
+      category: 'Seed'
+    },
+    {
+      name: 'Charmander',
+      height: 2,
+      types: ['fire'],
+      category: 'Lizard'
+    },
+    {
+      name: 'Charizard',
+      height: 5,
+      types: ['fire', 'flying'],
+      category: 'Flame'
+    }
+  ]
+  function add (pokemon) {
+    pokemonList.push(pokemon)
   }
-]
-pokemonList.forEach(function (pokemon) {
-  document.write('<p>' + pokemon.name, '(height: ' + pokemon.height + ')</p>')
+
+  function getAll () {
+    return pokemonList
+  }
+  return {
+    add: add,
+    getAll: getAll
+  }
 })()
+console.log(pokemonRepository.getAll())
+pokemonRepository.add({ name: 'Butterfree', height: 3, types: ['bug', 'flying'], category: 'Butterfly' })
+console.log(pokemonRepository.getAll())
+// Bonus Task
+Object.keys(pokemonRepository).forEach(function (item) {
+  console.log(pokemonRepository[item])
+})
